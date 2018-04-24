074bcef58b371c6f98d9475e27c95620f8a8dea6
httpservlet <httpservlet@yeah.net>
2018-04-24 11:48:45 +0800

    update API.MD. update account all interface

--- ulord/ulord/schema/transactions.py
+++ ulord/ulord/schema/transactions.py
@@ -7,7 +7,8 @@ from ulord.extensions import ma
 from ulord.models import Content,Consume
 
 _all__ = ['content_schema','contents_schema','content_consume_schema',
-          'content_consumes_schema','consumeinout_schema','consumeinouts_schema']
+          'content_consumes_schema','consumeinout_schema','consumeinouts_schema',
+          'content_publish_schema','content_publishs_schema']
 
 class ContentSchema(ma.ModelSchema):
     class Meta:
@@ -19,11 +20,17 @@ contents_schema=ContentSchema(many=True)
 
 class ContentConsumeSchema(ma.ModelSchema):
     class Meta:
-        fields=['content']
+        fields=['id','author','claim_id','title','txid','enabled','price', 'create_timed']
 
 content_consume_schema=ContentConsumeSchema()
 content_consumes_schema=ContentConsumeSchema(many=True)
 
+class ContenPublishSchema(ma.ModelSchema):
+    class Meta:
+        fields=['id','claim_id','title','txid','enabled','customer','price', 'create_timed']
+
+content_publish_schema=ContenPublishSchema()
+content_publishs_schema=ContenPublishSchema(many=True)
 
 class ConsumeSchema(ma.ModelSchema):
     class Meta:
