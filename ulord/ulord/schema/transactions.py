d6c2d01fcd1537a6ce1dae095c3f3522de6b2a5b
httpservlet <httpservlet@yeah.net>
2018-04-27 09:27:18 +0800

    update comment

--- ulord/ulord/schema/transactions.py
+++ ulord/ulord/schema/transactions.py
@@ -16,12 +16,13 @@ class TagSchema(ma.ModelSchema):
 
 class ContentSchema(ma.ModelSchema):
     class Meta:
-        model = Content
+        # model = Content
         fields = (
             'id', 'claim_id', 'author', 'title', 'price', 'content_type', 'currency', 'des', 'status', 'create_timed',
             'update_timed', 'views', 'tags', 'create_timed_timestamp', 'update_timed_timestamp')
         # exclude=('txid','enabled','consumes','ipfs_hash','appkey')
     # 参考 http://marshmallow.readthedocs.io/en/latest/nesting.html?highlight=Nested
+    # 如果是使用model,就不需要nested了, 内部实现应该做了处理
     tags = ma.Nested(TagSchema, many=True, only='name')
 
 content_schema = ContentSchema()
